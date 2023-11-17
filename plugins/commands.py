889ba51862d6ef4e1ff015d634a84bab444a6a22
Kanxothito <133907064+Kanxothito@users.noreply.github.com>
2023-11-18 02:49:26 +0545

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -447,7 +447,7 @@ async def start(client, message):
             pass
         return await message.reply('No such file exist.')
     files = files_[0]
-    title = 'Tech_support_channel  ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))
+    title = '@Tech_support_channel  ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))
     size=get_size(files.file_size)
     f_caption=files.caption
     if CUSTOM_FILE_CAPTION:
