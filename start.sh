63984ce6f45e7fc7d523dcaac40be94a75020dcf
Kanxothito <133907064+Kanxothito@users.noreply.github.com>
2023-11-18 00:20:20 +0545

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
+  git clone https://github.com/Kanxothito/Nep.git /Nep 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
+  git clone $UPSTREAM_REPO /Nep 
 fi
-cd /VJ-FILTER-BOT 
+cd /Nep 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
